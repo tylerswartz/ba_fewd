9709a866dc32f4b010b287842e0001f3dc71321e
tylerswartz <tgswartz@gmail.com>
2015-02-27 18:53:59 -0800

    started chapter 8

---
+++ hartl_ch8.rb
@@ -0,0 +1,17 @@
+# Bulding and sorting an array
+
+puts "Hi, I'm going to sort a-z any words you'd like. Start by typing your words, when you are done just press enter on an empty line. Ready...give me some words:"
+
+words = []
+
+x = gets
+puts x
+# while gets != '\n'
+# 	new_word = gets.chomp.upcase
+# 	words.push new_word
+# end
+
+# words.sort
+
+# puts words
+
