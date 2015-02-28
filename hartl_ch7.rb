5c73432e26502436d7305a04326741f243793139
tylerswartz <tgswartz@gmail.com>
2015-02-27 18:41:17 -0800

    finished ch 7 leap years problem

--- hartl_ch7.rb
+++ hartl_ch7.rb
@@ -19,27 +19,80 @@
 
 # deaf grandma
 
-puts "HIGH SONNY, HOW ARE YOU DOING?"
-x = 0
+# puts "HIGH SONNY, HOW ARE YOU DOING?"
+# x = 0
 
-while true
-	response = gets.chomp	
+# while true
+# 	response = gets.chomp	
 	
-	if response == 'BYE'
-		x = x + 1
-	else
-		x = 0
-	end
+# 	if response == 'BYE'
+# 		x = x + 1
+# 	else
+# 		x = 0
+# 	end
 	
-	if x == 3
-		puts "BYE BYE HONEY!"
-		break
-	end
+# 	if x == 3
+# 		puts "BYE BYE HONEY!"
+# 		break
+# 	end
 
-	if response == response.upcase
-		puts "OH...NO, NOT SINCE 19" + rand(10..60).to_s + "!"
-	else
-		puts "HUH?! SPEAK UP SONNY!"
+# 	if response == response.upcase
+# 		puts "OH...NO, NOT SINCE 19" + rand(10..60).to_s + "!"
+# 	else
+# 		puts "HUH?! SPEAK UP SONNY!"
+# 	end
+# end
+
+
+
+# leap years
+
+puts "I calculate leap years. Tell me two years and I'll tell you how many leap years are in between them. Give me the first year (lower year)..."
+year1 = gets.chomp.to_i
+puts "Great, now give me the second year (higher year)..."
+year2 = gets.chomp.to_i
+puts "Cool, the leaps years are:"
+
+
+while year1 < year2
+	year1 = year1 + 1
+	if ((year1/4.0) % 1 ) == 0 && ((year1/100.0) % 1 ) != 0
+		puts year1
+	elsif ((year1/100.0) % 1 ) == 0 && ((year1/400.0) % 1 ) == 0
+		puts year1
 	end
-end
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
