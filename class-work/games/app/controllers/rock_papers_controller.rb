85beecde27f53aa08e87bc82a8feed58309e8bad
tylerswartz <tgswartz@gmail.com>
2015-03-19 21:30:11 -0700

    setting up logic, created a compare_hands method in def show

--- class-work/games/app/controllers/rock_papers_controller.rb
+++ class-work/games/app/controllers/rock_papers_controller.rb
@@ -5,12 +5,24 @@ class RockPapersController < ApplicationController
   def show
     @player_throw = params[:id].to_i
     @robot_throw = rand(0..2)
-    @player_won = (@player_throw > @robot_throw)
+
+    compare_hands(@player_throw,@robot_throw)
+
 
     if @player_won
       @outcome_partial = 'win'
     else
       @outcome_partial = 'lose'
     end
+  end
+
+  def compare_hands(player,robot)
+  	if player == robot
+  		#outcome_partial = 'tie'.
+  		#need to create a tie partial. 
+  	elsif player = 0 && robot = 1
+  		#@outcome_partial = 'win'
+  	end
+
   end
 end
