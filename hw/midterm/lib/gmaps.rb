4673868f8048fa9ea1d8b246fd9347d6bb383150
tylerswartz <tgswartz@gmail.com>
2015-03-21 20:40:36 -0700

    finished google maps api for mid-term

--- hw/midterm/lib/gmaps.rb
+++ hw/midterm/lib/gmaps.rb
@@ -8,28 +8,44 @@ class GoogleMaps
 		ask_zipcode
 		request_api
 		output
+		ask_exit
 	end
 
 	def ask_zipcode
-		puts 'This is outputed information from the Google Maps API'
-		puts 'What US zipcode would you like to look up?'
-		zipcode = gets.chomp
+		puts "Enter a zipcode to find it's latitude and longitude..."
+		@zipcode = gets.chomp
 
-		while zipcode.to_i == 0
-	  puts "Thats not a Zipcode. Please try again"
-	  zipcode = gets.chomp
+		while zipcode.to_s.length != 5
+	  	puts "Thats not a valid zipcode. Please try again"
+	  	@zipcode = gets.chomp
 		end
 	end
 
 	def request_api
-		maps_api = JSON.load(RestClient.get("http://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode))
-		lat = maps_api['results'][0]['geometry']['location']['lat']
-		long = maps_api['results'][0]['geometry']['location']['lng']
+		maps_api = JSON.load(RestClient.get("http://maps.googleapis.com/maps/api/geocode/json?address=" + @zipcode))
+		@lat = maps_api['results'][0]['geometry']['location']['lat']
+		@long = maps_api['results'][0]['geometry']['location']['lng']
 	end
 	
 	def output
-		puts "Here is the latitude and longitude of " + zipcode.to_s + "..."
-		puts "Latitude: " + lat.to_s
-		puts "Longitude: " + long.to_s
+		puts "Here is the latitude and longitude of " + @zipcode.to_s + "..."
+		puts "Latitude: " + @lat.to_s
+		puts "Longitude: " + @long.to_s
+	end
+
+	def ask_exit
+		loop do
+			puts 'Would you like to check another zipcode? (y/n)'
+			answer = gets.chomp
+
+			if answer == 'y'
+	    	self.start
+	  	elsif answer == 'n'
+	    	puts "Thanks for using Google Maps API"
+	    	exit
+	  	else
+	    	puts "Please enter a valid choice option! (y/n)"
+	  	end
+	  end
 	end
 end
