ba408cc5177c0ffdd522336eabce27220ce3d31a
tylerswartz <tgswartz@gmail.com>
2015-04-02 06:36:19 -0700

    created Stories controller and Story model

---
+++ hw/Rewsly/app/controllers/stories_controller.rb
@@ -0,0 +1,7 @@
+class StoriesController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+end
--- hw/Rewsly/config/routes.rb
+++ hw/Rewsly/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'stories/new'
+
+  get 'stories/show'
+
+  get 'story/new'
+
+  get 'story/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
