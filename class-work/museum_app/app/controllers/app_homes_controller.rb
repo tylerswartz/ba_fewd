dd6a6b3776167237f32fa651d6136d9bab4a8049
tylerswartz <tgswartz@gmail.com>
2015-04-14 21:02:46 -0700

    added home controller

---
+++ class-work/museum_app/app/controllers/app_homes_controller.rb
@@ -0,0 +1,4 @@
+class AppHomesController < ApplicationController
+  def index
+  end
+end
--- class-work/museum_app/config/routes.rb
+++ class-work/museum_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'museums#index'
+  root 'app_homes#index'
 
+  resources :app_homes, only: [:index]
   resources :museums, except: [:destory]
   resources :artists, except: [:destory]
   resources :paintings, except: [:destory]
