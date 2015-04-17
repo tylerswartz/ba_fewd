d84d25898d8fc3e68216cf838d00816d2c8213cc
tylerswartz <tgswartz@gmail.com>
2015-04-16 18:30:20 -0700

    updated artist show page

--- class-work/museum_app/app/controllers/artists_controller.rb
+++ class-work/museum_app/app/controllers/artists_controller.rb
@@ -7,6 +7,8 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id])
+    @paintings = Painting.where(artist_id:(params[:id]))
   end
 
   def create
